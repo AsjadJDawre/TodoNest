3bb21b4fa4daf0dbaa0b27cecc415d2bd6f7c13d
Asjad Johar Dawre <dawreasjad72@gmail.com>
2025-02-11 08:55:37 +0530

    build Config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/TodoNest/',  // Replace 'TodoNest' with your actual GitHub repository name
+  base: '/',  // Replace 'TodoNest' with your actual GitHub repository name
 })
